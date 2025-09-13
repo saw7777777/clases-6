778467cce34ab6d4326c7397183881cbdd3c07d7
josue <firej390@gmail.con>
2025-09-13 17:57:57 -0400

    defini data_model

---
+++ blogs/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Post
+
+# Register your models here.
+class postAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at', 'updated_at')
+    search_fields = ('title', 'content', 'author__username')
+    list_filter = ('created_at', 'author')
+    ordering = ('-created_at',)
+
