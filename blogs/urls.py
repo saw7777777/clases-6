1d04de2ba4ad19e93f153111dffadac5f8f775c8
josue <firej390@gmail.con>
2025-09-20 17:47:51 -0400

    secun commit

--- blogs/urls.py
+++ blogs/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import postlistview, postdetailview
 
 urlpatterns = [
-    path('', postlistview.as_view(), name='post-list'),
-    path('post/<int:pk>/', postdetailview.as_view(), name='post-detail'),
+    path("", postlistview.as_view(), name="post-list"),
+    path("post/<int:pk>/", postdetailview.as_view(), name="post-detail"),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from .models import Post
 from django.views.generic import ListView, DetailView
+
 # Create your views here.
+
 
 class postlistview(ListView):
     model = Post
     template_name = "post_list.html"
+
 
 class postdetailview(DetailView):
     model = Post
