644f1b62346c4d49f0f1370a273c2111d6431ce4
josue <firej390@gmail.con>
2025-10-11 16:02:05 -0400

    se a agrgado el signup

--- django_base/urls.py
+++ django_base/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("blogs.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("accounts.urls")),
 ]
